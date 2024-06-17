2fee5f3bacc7616debd97ebd107648494b523b97
Muhammed Yasin <muhammedyasin8622@gmail.com>
2024-06-17 19:01:25 +0530

    updated contact

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,20 +1,19 @@
 import project1 from "../assets/projects/sellzy.png";
 import project2 from "../assets/projects/Task-Management.png";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/netflix.png";
-import project5 from "../assets/projects/olx.png";
+import project3 from "../assets/projects/netflix.png";
+import project4 from "../assets/projects/olx.png";
 
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a skilled fresher with a deep dedication to coding, investing the past 10 months in mastering the intricacies of the MERN stack (MongoDB, Express.js, React, and Node.js), alongside comprehensive studies in data structures and PostgreSQL. Building and refining projects has not only been a learning experience but a passion that drives my continuous improvement. Beyond coding, I find relaxation in watching movies and actively engaging in football matches. These activities not only complement my focused approach to learning and problem-solving but also enrich my ability to collaborate and communicate effectively in diverse environments. I am enthusiastic about leveraging my skills and experiences to tackle new challenges and contribute meaningfully to innovative projects.`;
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
     role: "Student",
     company: "Brototype",
-    description: `Stud.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    description: `I am currently enrolled in a bootcamp that emphasizes self-study, where I have been learning and mastering the MERN stack over the past 10 months. During this time, I have developed a thorough understanding of MongoDB, Express.js, React, and Node.js. Additionally, I have gained knowledge in data structures and PostgreSQL. I have applied my skills by building several projects, and I continue to enhance my expertise. As a result, I am now a skilled fresher ready to contribute to challenging projects.`,
+    technologies: [ "MERN" ,"Javascript"  ,"PostgresSQL" ,"Data Structures" , "Tailwind CSS"],
   }
 ];
 
@@ -37,7 +36,7 @@ export const PROJECTS = [
   },
   {
     title: "Netflix Clone",
-    image: project4,
+    image: project3,
     description:
       "An exact clone of netflix  where we can watch movie trailers and upcoming movies integrated with apis.",
     technologies: ["Tailwind CSS", "React", "FireBase" , "Vite"],
@@ -45,7 +44,7 @@ export const PROJECTS = [
   },
   {
     title: "Olx Clone",
-    image: project5,
+    image: project4,
     description:
       "An exact clone of olx  where we can sell and buy diffrent types of products.",
     technologies: ["Tailwind CSS", "React", "FireBase" , "Vite"],
